ade80e819ef5e4002c79f07d9f7ede6ed0e6586b
Alain Rodriguezz <macbookpro@MacBook-Pro-de-Alain.local>
2018-06-11 20:13:19 -0500

    Connect and Disconnect IO

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
-const path = require('path')
-const publicPath = path.join(__dirname,'./../public')
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.static(publicPath))
+const path = require('path');
+const socketIO = require('socket.io');
+const express = require('express');
+const http = require('http');
 
 
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'./../public');
 
 
-app.listen(port,()=>{
-	console.log(`Listening on ${port}`)
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+app.use(express.static(publicPath));
+
+
+io.on('connection',(socket)=>{
+	console.log('New user connected')
+
+
+
+	socket.on('disconnect',()=>{
+		console.log('User disconnected')	
+	})
+})
+
+
+
+server.listen(port,()=>{
+	console.log(`Listening on ${port}`);
 })
